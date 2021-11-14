deb865af75624c1d27f76c4fe1f14ba6bff022a0
Filip Lipowiecki <fili@softwaremind.com>
2021-11-14 18:57:37 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use playwright::Playwright;
+
+#[tokio::main]
+async fn main() -> Result<(), playwright::Error> {
+    let playwright = Playwright::initialize().await?;
+    playwright.prepare()?; // Install browsers
+    let chromium = playwright.chromium();
+    let browser = chromium.launcher().headless(true).launch().await?;
+    let context = browser.context_builder().build().await?;
+    let page = context.new_page().await?;
+    page.goto_builder("https://example.com/").goto().await?;
+
+    // Exec in browser and Deserialize with serde
+    let s: String = page.eval("() => location.href").await?;
+
+    println!("{}", s);
+    assert_eq!(s, "https://example.com/");
+    page.click_builder("a").click().await?;
+    let s2: String = page.eval("() => location.href").await?;
+    println!("{}", s2);
+    Ok(())
+}
